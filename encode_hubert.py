ecea6fb336f05aa1cf8de72f5807ba8f7ec5d9f7
Herman Kamper <kamperh@gmail.com>
2023-11-27 09:07:18 +0200

    HuBERT encoding

--- encode_hubert.py
+++ encode_hubert.py
@@ -52,6 +52,8 @@ def main():
     output_dir.mkdir(parents=True, exist_ok=True)
     (output_dir/"indices").mkdir(parents=True, exist_ok=True)
     (output_dir/"prequant").mkdir(parents=True, exist_ok=True)
+    (output_dir/"layer09").mkdir(parents=True, exist_ok=True)
+    # (output_dir/"layer10").mkdir(parents=True, exist_ok=True)
 
     # Model
     hubert = torch.hub.load(
@@ -66,15 +68,19 @@ def main():
     np.save(output_fn, codebook)
 
     n_min_samples = 640
+    n_max_samples = 2000000
     print("Writing to:", output_dir)
-    for wav_fn in tqdm(sorted(list(wav_dir.rglob("*.wav")))):
+    for i, wav_fn in enumerate(tqdm(sorted(list(wav_dir.rglob("*.wav"))))):
     # for wav_fn in ["wav/buckeye/val/s18_01b_004469-004474.wav",]:
 
         # Input
         wav, sr = torchaudio.load(wav_fn)
         assert sr == 16000
         l = wav.shape[1]
-        if l < n_min_samples:
+
+        if l > n_max_samples:
+            wav = wav[:, :n_max_samples]
+        elif l < n_min_samples:
             wav = F.pad(wav, (0, n_min_samples - l))
         x = wav.unsqueeze(0).cuda()
         x = F.pad(x, ((400 - 320) // 2, (400 - 320) // 2))
@@ -83,12 +89,22 @@ def main():
         prequant, _ = hubert.encode(x, layer=7)
         prequant = prequant.detach().squeeze().cpu().numpy()
 
+        # Higher layers
+        layer09, _ = hubert.encode(x, layer=9)
+        layer09 = layer09.detach().squeeze().cpu().numpy()
+        # layer10, _ = hubert.encode(x, layer=10)
+        # layer10 = layer10.detach().squeeze().cpu().numpy()
+
         # Extract units
         units = hubert.kmeans.predict(prequant)
 
-        # Write features
+        # # Write features
         output_fn = (output_dir/"prequant"/wav_fn.stem).with_suffix(".npy")
         np.save(output_fn, prequant)
+        output_fn = (output_dir/"layer09"/wav_fn.stem).with_suffix(".npy")
+        np.save(output_fn, layer09)
+        # output_fn = (output_dir/"layer10"/wav_fn.stem).with_suffix(".npy")
+        # np.save(output_fn, layer10)
 
         # Write units
         output_fn = (output_dir/"indices"/wav_fn.stem).with_suffix(".txt")
